764b092a9490974d31c9e10ce8ad7e75f9a0de31
Evan Hedbor <evan@hedbor.org>
2020-01-24 22:28:26 +0100

    Fixed everything

--- NightManaBar/Config.lua
+++ NightManaBar/Config.lua
@@ -4,14 +4,16 @@ local AceConfigDialog = LibStub("AceConfigDialog-3.0")
 local AceDB = LibStub("AceDB-3.0")
 local AceDBOptions = LibStub("AceDBOptions-3.0")
 
+local TimeSelectorWidth = 0.333
+
 local Hours = {}
 for i = 0, 23 do
-    Hours[i] = i
+    Hours[i] = format("%02d", i)
 end
 
 local Minutes = {}
-for i = 0, 55, 5 do 
-    Minutes[i] = i
+for i = 0, 55, 5 do
+    Minutes[i] = format("%02d", i)
 end
 
 NightManaBar.options = {
@@ -19,172 +21,135 @@ NightManaBar.options = {
     name = "NightManaBar",
     handler = NightManaBar,
     args = {
-        nightManaBarColor = {
-            type = "color",
-            name = "Mana Bar Color (Night)",
-            order = 20,
-            width = "full",
-            get = "GetNightManaBarColor",
-            set = "SetNightManaBarColor"  
-        },
-        sunrise = {
+        general = {
             type = "group",
-            name = "Sunrise Time",
-            order = 30,
-            inline = true,
+            name = "General",
             args = {
-                hour = {
+                nightManaBarColor = {
+                    type = "color",
+                    name = "Mana Bar Color (Night)",
+                    order = 1100,
+                    width = "full",
+                    get = "GetNightManaBarColor",
+                    set = "SetNightManaBarColor"
+                },
+                sunriseLabel = {
+                    type = "description",
+                    name = "Sunrise Time",
+                    order = 2110,
+                    fontSize = "medium"
+                },
+                sunriseHour = {
                     type = "select",
                     name = "Hour",
+                    order = 2120,
+                    width = TimeSelectorWidth,
                     values = Hours,
                     get = "GetSunriseHour",
                     set = "SetSunriseHour"
                 },
-                minute = {
+                sunriseMinute = {
                     type = "select",
                     name = "Minute",
+                    order = 2130,
+                    width = TimeSelectorWidth,
                     values = Minutes,
                     get = "GetSunriseMinute",
                     set = "SetSunriseMinute"
-                }
-            }
-        },
-        sunset = {
-            type = "group",
-            name = "Sunset Time",
-            order = 40,
-            inline = true,
-            args = {
-                hour = {
+                },
+                sunsetLabel = {
+                    type = "description",
+                    name = "Sunset Time",
+                    order = 2210,
+                    fontSize = "medium"
+                },
+                sunsetHour = {
                     type = "select",
                     name = "Hour",
+                    order = 2220,
+                    width = TimeSelectorWidth,
                     values = Hours,
                     get = "GetSunsetHour",
                     set = "SetSunsetHour"
                 },
-                minute = {
+                sunsetMinute = {
                     type = "select",
                     name = "Minute",
+                    order = 2230,
+                    width = TimeSelectorWidth,
                     values = Minutes,
                     get = "GetSunsetMinute",
                     set = "SetSunsetMinute"
+                },
+                is24HourMode = {
+                    type = "toggle",
+                    name = "24 Hour Mode",
+                    order = 2310,
+                    width = "full",
+                    disabled = true,
+                    get = function() return true end,
+                    set = function() end
+                },
+                useLocalTime = {
+                    type = "toggle",
+                    name = "Use Local Time",
+                    order = 2320,
+                    width = "full",
+                    get = "GetUseLocalTime",
+                    set = "SetUseLocalTime"
+                },
+                resetHeader = {type = "header", name = "Reset NightManaBar", order = 3210},
+                reset = {
+                    type = "execute",
+                    name = "Reset to Defaults",
+                    desc = "Clicking this button will reset all options to their default values.",
+                    order = 3220,
+                    confirm = true,
+                    func = "ResetConfig"
                 }
             }
-        },
-        useLocalTime = {
-            type = "toggle",
-            name = "Use local time",
-            desc = "If enabled, use local time instead of server time.",
-            order = 50,
-            width = "full",
-            get = "GetUseLocalTime",
-            set = "SetUseLocalTime"
-        },
-        updateColor = {
-            type = "execute",
-            name = "Update Color",
-            desc = "Click this to manually update the mana bar's color.",
-            order = 55,
-            func = "UpdateManaBarColor"
-        },
-        resetHeader = {
-            type = "header",
-            name = "Reset NightManaBar",
-            order = 57
-        },
-        reset = {
-            type = "execute",
-            name = "Reset to Defaults",
-            desc = "Clicking this button will reset all options to their default values.",
-            order = 60,
-            confirm = true,
-            func = "ResetConfig"
         }
     }
 }
 
 NightManaBar.defaults = {
     profile = {
-        nightManaBarColor = { r = 100/255, g = 100/255, b = 1.00 },
-        sunrise = {
-            hour = 5,
-            minute = 30
-        },
-        sunset = {
-            hour = 21,
-            minute = 0 
-        },
-        useLocalTime = false,
+        nightManaBarColor = {r = 100 / 255, g = 100 / 255, b = 1.00},
+        sunrise = {hour = 5, minute = 30},
+        sunset = {hour = 21, minute = 0},
+        useLocalTime = false
     }
 }
 
-function NightManaBar:RegisterConfig() 
+function NightManaBar:RegisterConfig()
     self.db = AceDB:New("NightManaBarDB", self.defaults)
-    
+
     AceConfig:RegisterOptionsTable("NightManaBar", self.options)
     AceConfigDialog:AddToBlizOptions("NightManaBar")
+
+    self.options.args.profiles = AceDBOptions:GetOptionsTable(self.db)
 end
 
+function NightManaBar:ResetConfig(info) self.db:ResetProfile() end
 
-function NightManaBar:ResetConfig(info) 
-    self.db:ResetProfile()
-    --self:UpdateManaBarColor()
+function NightManaBar:GetNightManaBarColor(info)
+    local color = self.db.profile.nightManaBarColor
+    return color.r, color.g, color.b, 1.0
 end
 
--- function NightManaBar:GetDayManaBarColor(info)
---     local color = self.db.profile.dayManaBarColor
---     return color.r, color.g, color.b, 1.0
--- end
-
--- function NightManaBar:SetDayManaBarColor(info, r, g, b) 
---     self.db.profile.dayManaBarColor = {r=r, g=g, b=b} 
--- end
-
-function NightManaBar:GetNightManaBarColor(info) 
-    local color = self.db.profile.nightManaBarColor 
-    return color.r, color.g, color.b, 1.0 
+function NightManaBar:SetNightManaBarColor(info, r, g, b)
+    self.db.profile.nightManaBarColor = {r = r, g = g, b = b}
 end
 
-function NightManaBar:SetNightManaBarColor(info, r, g, b) 
-    self.db.profile.nightManaBarColor = {r=r, g=g, b=b}
-end
+function NightManaBar:GetSunriseHour(info) return self.db.profile.sunrise.hour end
+function NightManaBar:SetSunriseHour(info, value) self.db.profile.sunrise.hour = value end
+function NightManaBar:GetSunriseMinute(info) return self.db.profile.sunrise.minute end
+function NightManaBar:SetSunriseMinute(info, value) self.db.profile.sunrise.minute = value end
 
-function NightManaBar:GetSunriseHour(info) 
-    return self.db.profile.sunrise.hour
-end
+function NightManaBar:GetSunsetHour(info) return self.db.profile.sunset.hour end
+function NightManaBar:SetSunsetHour(info, value) self.db.profile.sunset.hour = value end
+function NightManaBar:GetSunsetMinute(info) return self.db.profile.sunset.minute end
+function NightManaBar:SetSunsetMinute(info, value) self.db.profile.sunset.minute = value end
 
-function NightManaBar:SetSunriseHour(info, value) 
-    self.db.profile.sunrise.hour = value 
-end
-
-function NightManaBar:GetSunriseMinute(info) 
-    return self.db.profile.sunrise.minute 
-end
-
-function NightManaBar:SetSunriseMinute(info, value) 
-    self.db.profile.sunrise.minute = value 
-end
-
-function NightManaBar:GetSunsetHour(info) 
-    return self.db.profile.sunset.hour 
-end
-
-function NightManaBar:SetSunsetHour(info, value) 
-    self.db.profile.sunset.hour = value 
-end
-
-function NightManaBar:GetSunsetMinute(info) 
-    return self.db.profile.sunset.minute 
-end
-
-function NightManaBar:SetSunsetMinute(info, value) 
-    self.db.profile.sunset.minute = value 
-end
-
-function NightManaBar:GetUseLocalTime(info) 
-    return self.db.profile.useLocalTime 
-end
-
-function NightManaBar:SetUseLocalTime(info, value) 
-    self.db.profile.useLocalTime = value 
-end
+function NightManaBar:GetUseLocalTime(info) return self.db.profile.useLocalTime end
+function NightManaBar:SetUseLocalTime(info, value) self.db.profile.useLocalTime = value end
--- NightManaBar/Core.lua
+++ NightManaBar/Core.lua
@@ -2,32 +2,38 @@ local NightManaBar = LibStub("AceAddon-3.0"):NewAddon("NightManaBar",
     "AceConsole-3.0", "AceEvent-3.0", "AceTimer-3.0")
 
 local ManaBars = {
-    player = PlayerFrameManaBar, 
+    player = PlayerFrameManaBar,
     pet = PetFrameManaBar,
-    target = TargetFrameManaBar, 
+    target = TargetFrameManaBar,
     targettarget = TargetFrameToTManaBar, -- Broken? Changing this color has no effect.
-    party1 = PartyMemberFrame1ManaBar, 
-    party2 = PartyMemberFrame2ManaBar, 
-    party3 = PartyMemberFrame3ManaBar, 
+    party1 = PartyMemberFrame1ManaBar,
+    party2 = PartyMemberFrame2ManaBar,
+    party3 = PartyMemberFrame3ManaBar,
     party4 = PartyMemberFrame4ManaBar
 }
 
-function NightManaBar:OnInitialize() 
+local RegisteredEvents = {
+    -- "PLAYER_ENTERING_WORLD",
+    -- "PLAYER_TARGET_CHANGED",
+    -- "UPDATE_SHAPESHIFT_FORM",
+    "UNIT_DISPLAYPOWER", -- Called when the power type changes
+    "UNIT_MAXPOWER", -- Called when the max power changes
+    "UNIT_POWER_UPDATE" -- Called when the current power updates 
+}
+
+function NightManaBar:OnInitialize()
     self:RegisterConfig()
-    self:RegisterEvent("PLAYER_ENTERING_WORLD", "UpdateManaBarColor")
-    self:RegisterEvent("PLAYER_TARGET_CHANGED", "UpdateManaBarColor")
-    self:RegisterEvent("UNIT_DISPLAYPOWER", "UpdateManaBarColor")
-    self:RegisterEvent("UPDATE_SHAPESHIFT_FORM", "UpdateManaBarColor")
+    for _, event in ipairs(RegisteredEvents) do
+        self:RegisterEvent(event, "UpdateManaBarColor")
+    end
 end
 
-function NightManaBar:OnEnable() 
+function NightManaBar:OnEnable()
     self:UpdateManaBarColor()
-    self.timer = self:ScheduleRepeatingTimer("UpdateManaBarColor", 5)
+    self.timer = self:ScheduleRepeatingTimer("UpdateManaBarColor", 1)
 end
 
-local function GetMinutes(hour, minute)
-    return 60 * hour + minute  
-end
+local function GetMinutes(hour, minute) return 60 * hour + minute end
 
 local function IsTimeBetween(time, start, stop)
     -- See https://stackoverflow.com/questions/35402992/how-to-check-if-current-time-is-between-two-others
@@ -37,10 +43,10 @@ local function IsTimeBetween(time, start, stop)
 
     -- add 24 hours if endhours < starthours
     if (StopH < StartH) then
-        local StopHOrg=StopH
+        local StopHOrg = StopH
         StopH = StopH + 24
         -- if endhours has increased the currenthour should also increase
-        if (TestH <= StopHOrg) then 
+        if (TestH <= StopHOrg) then
             TestH = TestH + 24
         end
     end
@@ -48,25 +54,37 @@ local function IsTimeBetween(time, start, stop)
     local StartTVal = GetMinutes(StartH, StartM)
     local StopTVal = GetMinutes(StopH, StopM)
     local curTVal = GetMinutes(TestH, TestM)
-    return (curTVal >= StartTVal and curTVal <= StopTVal) 
-end 
+    return (curTVal >= StartTVal and curTVal <= StopTVal)
+end
 
-function NightManaBar:UpdateManaBarColor() 
-    local isNight = not IsTimeBetween(self:GetCurrentTime(), self.db.profile.sunrise, self.db.profile.sunset)
+function NightManaBar:UpdateManaBarColor()
+    local isNight = not IsTimeBetween(
+        self:GetCurrentTime(), self.db.profile.sunrise, self.db.profile.sunset)
     for unitid, manaBar in pairs(ManaBars) do
         local _, powerToken = UnitPowerType(unitid)
         if powerToken ~= nil then
             local c = PowerBarColor[powerToken]
-            if isNight and powerToken == "MANA" then 
+            if isNight and powerToken == "MANA" then
                 c = self.db.profile.nightManaBarColor
             end
-            manaBar:SetStatusBarColor(c.r, c.g, c.b) 
+            manaBar:SetStatusBarColor(c.r, c.g, c.b)
         end
     end
 end
 
 function NightManaBar:GetCurrentTime()
-    local datetime = 
-        self.db.profile.useLocalTime and date("*t") or date("*t", GetServerTime())
-    return { hour = datetime.hour, minute = datetime.min }
-end 
+    local hour, minute
+    if self.db.profile.useLocalTime then
+        local time = date("*t")
+        hour = time.hour
+        minute = time.min
+    else
+        -- Note: GetServerTime returns the unix timestamp of the server. Unix 
+        -- time is TIMEZONE INDEPENDENT! Using date() to convert it will simply 
+        -- return the LOCAL TIME, as measured by the server!
+        -- local time = date("*t", GetServerTime())
+        hour, minute = GetGameTime()
+    end
+
+    return {hour = hour, minute = minute}
+end
